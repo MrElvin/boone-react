ee4d05fec29fd7eed8d0ab589e504de64d694458
Breezymelon <935448078@qq.com>
2019-05-04 19:40:31 +0800

    fix(views/): fix conflict

--- src/views/About/index.jsx
+++ src/views/About/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 class About extends Component {
-  render() {
+  render () {
     return (
       <div>
         Welcome to <strong>Test About</strong> page!
